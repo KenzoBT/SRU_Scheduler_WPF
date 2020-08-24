dff7f04e01066992ad5f4e2824b2421cb5315d5c
KenzoBT <jkb1015@sru.edu>
2020-08-24 19:23:53 -0400

    Temp Stable Build for x64

--- Schedule_WPF/MainWindow.xaml.cs
+++ Schedule_WPF/MainWindow.xaml.cs
@@ -579,7 +579,7 @@ namespace Schedule_WPF
             //MessageBox.Show("ColorIndex is currently: " + Settings.Default.ColorIndex);
             // Read from Colors file to see which professors we have already assigned a color. Store in colorPairings List.
             string tempPath = System.IO.Path.GetTempPath();
-            string filename = "ColorConfigurations13.xml";
+            string filename = "ColorConfigurations14.xml";
             colorFilePath = System.IO.Path.Combine(tempPath, filename);
             XmlSerializer ser = new XmlSerializer(typeof(Pairs));
             if (!File.Exists(colorFilePath))
